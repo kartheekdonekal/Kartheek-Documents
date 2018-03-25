dcc3be8e1c1038ac0cfc95a0ea3509798346576a
Kartheek Chowdary <Chowdarykarthik425@gmail.com>
2018-03-25 16:58:16 +0530

    commit

---
+++ GitProject/src/com/gitproject/selenium/GitTest1.java
@@ -0,0 +1,12 @@
+package com.gitproject.selenium;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		
+	System.out.println("My Name is kartheek");
+	System.out.println("My Brother Name is Jeevan");
+
+	}
+
+}
